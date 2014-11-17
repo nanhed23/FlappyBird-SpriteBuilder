3169041bd193cacb780e85a6bb46cb34ed63f111
Jenjira Holmes <jjenjirah@gmail.com>
2014-11-17 04:03:49 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,11 +11,24 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    
+    timeSinceObstacle = 0.0f;
+    
+- (void)update:(CCTime)delta
+    {
+        // this will be run every frame.
+        // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+    }
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+- (void)update:(CCTime)delta
+    {
+        // this will be run every frame.
+        // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+    }
 }
 
 // put new methods here
